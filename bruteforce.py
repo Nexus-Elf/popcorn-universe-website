c63e76c6b811ee3496fb269190bc8b9b7e417414
NexusSZN <damianbennett2703@gmail.com>
2020-10-13 10:25:28 -0400

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -1,20 +1,15 @@
+
+
 import random
 import pyautogui
 
-UsableChars = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+UsableChars = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ.-+="
 chars_list = list(UsableChars)
 password = pyautogui.password("Please enter a password : " )
 guess_password = ""
-while(guess_password != password);
-guess_password = random.choices(chars_list, k=len(password))
-print("[-----="+ str(guess_password)+ "=-----]")
-if(guess_password == list(password));
-print("Your Password is: " + "".join(guess_password))
-
-
-
-
-break
-
-
-
+while guess_password != password:
+  guess_password = random.choices(chars_list, k=len(password))
+  print("[-----="+ str(guess_password)+ "=-----]")
+  if(guess_password == list(password)) :
+    print("Your Password is: " + "".join(guess_password))
+    break
